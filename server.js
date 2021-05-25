6e1b8c16d75b0934a97a20c3b41b58ac801421fd
Diubi <alessandroogioiaa@gmail.com>
2021-05-25 22:39:52 +0200

    Sistema di registrazione, no alerts

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const connect = require('connect');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const port = 3000;
 
-const serveStatic = require('serve-static');
 const app = express();
 
 app.use( bodyParser.json() );
@@ -13,16 +12,41 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/styles", express.static(path.join(__dirname, '/styles')));
+app.use("/js", express.static(path.join(__dirname, '/js')));
 
 app.get('/', function(req, res){
     res.render('index', {});
 });
 
-app.post('/signup', function(req, res){
-    console.log(req.body.username);
-    console.log(req.body.password);
-});
+app.post('/signup', (req, res) => {
+    console.log("Nickname: " + req.body.nickname)
+    console.log("Password: " + req.body.password)
+
+    let nickname = req.body.nickname;
+    let password = req.body.password;
+
+    if(!nickname && !password){
+        return;
+    }
+    else if(fs.existsSync(nickname + ".json")){
+        return;
+    }
+    else{
+        let user = {
+            nickname: req.body.nickname,
+            password: req.body.password
+        }
+    
+        let json = JSON.stringify(user);
+        fs.writeFile(req.body.nickname + ".json", json, function(err){
+            if(err){
+                return console.error(err);
+            }
+        });
+        return;
+    }
+})
 
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname);
