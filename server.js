7fa5c5d5c67ba39a7508e4f928bd08c9f1e1f7bf
Diubi <alessandroogioiaa@gmail.com>
2021-05-26 21:02:31 +0200

    Regex

--- server.js
+++ server.js
@@ -27,11 +27,18 @@ app.post('/signup', function (req, res) {
     console.log("Nickname: " + req.body.nickname);
     console.log("Password: " + req.body.password);
 
-    var nickname = req.body.nickname;
-    var password = req.body.password;
+    var nickname = req.body.nickname.trim();
+    var password = req.body.password.trim();
 
-    if (fs.existsSync("accounts/" + nickname + ".json")) {
-        res.render('signuperror.html', {result: "This user already exists."}, function(err,html){
+    var regex = /\W|_/g;
+
+    if(regex.test(nickname)){
+        res.render('signup.html', {msg: "Only alphanumeric characters are allowed."}, function(err,html){
+            res.send(html);
+        });
+    }
+    else if (fs.existsSync("accounts/" + nickname + ".json")) {
+        res.render('signup.html', {msg: "This user already exists."}, function(err,html){
             res.send(html);
         });
     }
@@ -44,10 +51,10 @@ app.post('/signup', function (req, res) {
         var json = JSON.stringify(user);
         fs.writeFile("accounts/" + req.body.nickname + ".json", json, function (err) {
             if (err) {
-                throw console.error(err);
+                return console.error(err);
             }
         });
-        res.render('signup.html', {result: "This user already exists."}, function(err,html){
+        res.render('signup.html', {msg: "You signed up!"}, function(err,html){
             res.send(html);
         });
     }
